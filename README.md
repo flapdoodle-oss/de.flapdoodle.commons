059d08d29a330578fe5ada50e33c605976f4b78e
Michael Mosmann <michael@mosmann.de>
2026-08-03 07:25:20 +0200

    checks module

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# de.flapdoodle.commos
+# Organisation Flapdoodle OSS
+
+We are a github organisation. You are invited to participate.
+
+## de.flapdoodle.commos
+
 flapdoodle commons - a package of java utility classes
---
+++ checks/src/test/java/de/flapdoodle/checks/PreconditionsTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2016
+ *   Michael Mosmann <michael@mosmann.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.flapdoodle.checks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PreconditionsTest {
+
+	@Test
+	public void emptyArgsMustGiveMessage() {
+		assertEquals("foo", Preconditions.format("foo"));
+	}
+
+	@Test
+	public void emptyArgsWithPlaceholderMustGiveMessage() {
+		assertEquals("foo %s", Preconditions.format("foo %s"));
+	}
+
+	@Test
+	public void oneArgMustGiveMessageWithArg() {
+		assertEquals("foo bar", Preconditions.format("foo %s", "bar"));
+	}
+
+	@Test
+	public void oneArgWithoutPlaceholderMustGiveMessage() {
+		assertEquals("foo,bar", Preconditions.format("foo", "bar"));
+	}
+
+	@Test
+	public void oneMoreArgThanPlaceholderMustGiveArgAppendedToTheEnd() {
+		assertEquals("foo bar,blub", Preconditions.format("foo %s", "bar", "blub"));
+	}
+
+	@Test
+	public void oneMorePlaceholderThanArgMustGiveEmpty() {
+		assertEquals("foo bar blub <arg2>", Preconditions.format("foo %s %s %s", "bar", "blub"));
+	}
+
+	@Test
+	public void lazyArgumentWillBeEvaluatedLazy() {
+		assertEquals("normal: A, lazy: B", Preconditions.format("normal: %s, lazy: %s", "A", Preconditions.lazy(() -> "B")));
+	}
+
+	@Test
+	public void argumentFalseWillFailWithIllegalArgument() {
+		assertThatThrownBy(() -> Preconditions.checkArgument(false, "foo"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void argumentTrueWillNotFail() {
+		assertThat(Preconditions.checkArgument(true, "foo")).isTrue();
+	}
+
+	@Test
+	public void checkNullWillFailWithIllegalArgument() {
+		assertThatThrownBy(() -> Preconditions.checkNotNull(null, "foo"))
+			.isInstanceOf(NullPointerException.class);
+	}
+
+	@Test
+	public void checkStateWillFailWithIllegalStateException() {
+		assertThatThrownBy(() -> Preconditions.checkState(false, "foo"))
+			.isInstanceOf(IllegalStateException.class);
+	}
+
+	@Test
+	public void checkPresentWillFailWithIllegalArgument() {
+		assertThatThrownBy(() -> Preconditions.checkPresent(Optional.empty(), "foo"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	public void checkNegativeIndexWillFailWithIndexOutOfBoundsException() {
+		assertThatThrownBy(() -> Preconditions.checkElementIndex(-1,12,"name"))
+			.isInstanceOf(IndexOutOfBoundsException.class)
+			.hasMessageContaining("name (-1) must not be negative");
+	}
+
+	@Test
+	public void checkToBigIndexWillFailWithIndexOutOfBoundsException() {
+		assertThatThrownBy(() -> Preconditions.checkElementIndex(13,12,"name"))
+			.isInstanceOf(IndexOutOfBoundsException.class)
+			.hasMessageContaining("name (13) must be less than size (12)");
+	}
+
+	@Test
+	public void checkNegativeSizeIndexWillFailWithIllegalArgumentException() {
+		assertThatThrownBy(() -> Preconditions.checkElementIndex(13,-1,"name"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("negative size: -1");
+	}
+
+	@Test
+	public void checkNullWillSuccessWithValue() {
+		assertThat(Preconditions.checkNotNull("bar", "foo")).isEqualTo("bar");
+	}
+
+	@Test
+	public void checkStateTrueWillNotFail() {
+		assertThat(Preconditions.checkState(true, "foo")).isTrue();
+	}
+
+	@Test
+	public void checkPresentWillSuccessWithPresentValue() {
+		assertThat(Preconditions.checkPresent(Optional.of("bar"), "foo")).contains("bar");
+	}
+
+	@Test
+	public void checkIndexWithUpperAndLowerBound() {
+		assertThat(Preconditions.checkElementIndex(0, 2)).isEqualTo(0);
+		assertThat(Preconditions.checkElementIndex(1, 2)).isEqualTo(1);
+	}
+}
